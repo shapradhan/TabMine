db41109c2cabde040b5f8edaf9144d29f0245e06
shapradhan <shapradhan@users.noreply.github.com>
2023-07-28 20:12:58 +0200

    Create a function to get group embeddings

---
+++ enriched_community_detector.py
@@ -0,0 +1,10 @@
+import spacy
+import numpy as np
+nlp = spacy.load('en_core_web_lg')
+
+def get_group_embeddings(group_texts):
+    group_embeddings = []
+    for text in group_texts:
+        doc = nlp(text)
+        texts_embedding = np.mean([token.vector for token in doc], axis=0)
+        group_embeddings.append(texts_embedding)
