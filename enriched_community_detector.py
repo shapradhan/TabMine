93593528575bf847b0d1e0fa4d16222db166b392
shapradhan <shapradhan@users.noreply.github.com>
2023-07-28 20:13:20 +0200

    Create a function to get average of the group embeddings

--- enriched_community_detector.py
+++ enriched_community_detector.py
@@ -8,3 +8,6 @@ def get_group_embeddings(group_texts):
         doc = nlp(text)
         texts_embedding = np.mean([token.vector for token in doc], axis=0)
         group_embeddings.append(texts_embedding)
+
+def get_average_embedding(group_embeddings):
+    return np.mean(group_embeddings, axis=0)
