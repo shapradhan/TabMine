3b37f2deec05a9bc0115f40adce5f17fff1413e0
shapradhan <shapradhan@users.noreply.github.com>
2023-07-28 20:05:20 +0200

    Create a function to get edges

--- utils.py
+++ utils.py
@@ -5,6 +5,12 @@ def _get_parent_node(fk_rel):
     end_str = '('
     result = _get_word_between_strings(fk_rel[2], start_str, end_str).strip()
     return result
+
+def get_edges(fk_rel):
+    child_node = fk_rel[0]
+    parent_node = _get_parent_node(fk_rel, child_node)
+    return (parent_node, child_node)
+
 def _get_word_between_strings(text, start_str, end_str):
     pattern = re.compile(rf"{re.escape(start_str)}(.*?){re.escape(end_str)}")
     match = pattern.search(text)
