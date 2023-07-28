3746981d4bbe6e3f093fa28b335c4f6ab22af994
shapradhan <shapradhan@users.noreply.github.com>
2023-07-28 20:10:26 +0200

    Get nodes and edges from all foreign key relationships

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from dotenv import load_dotenv
 from os import getenv
+
+from data_extractor import get_all_fks, get_table_names
+from db_connector import connect
+from utils import get_edges, draw_graph
 if __name__ == '__main__':
     load_dotenv()
     
     DB_CONFIG_FILE = getenv('DB_CONFIG_FILE')
     SECTION = getenv('SECTION') 
+    
+    cur = connect(filename=DB_CONFIG_FILE, section=SECTION)
+    all_fk_rels = get_all_fks(cur)
+    
+    nodes = [i[0] for i in get_table_names(cur)]
+    edges = [get_edges(i) for i in all_fk_rels]
