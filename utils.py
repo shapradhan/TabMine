b6a57839724c035c1ee4c5debec5573f3cca63c3
shapradhan <shapradhan@users.noreply.github.com>
2023-07-28 20:04:58 +0200

    Create a function to get parent table from the foreign key relation

--- utils.py
+++ utils.py
@@ -1,4 +1,10 @@
 import re
+
+def _get_parent_node(fk_rel):
+    start_str = 'REFERENCES'
+    end_str = '('
+    result = _get_word_between_strings(fk_rel[2], start_str, end_str).strip()
+    return result
 def _get_word_between_strings(text, start_str, end_str):
     pattern = re.compile(rf"{re.escape(start_str)}(.*?){re.escape(end_str)}")
     match = pattern.search(text)
