ab88280a2162a8c35c2c1310a0c22531d0c92445
shapradhan <shapradhan@users.noreply.github.com>
2023-08-01 17:41:18 +0200

    Clean code and add docstring

--- sequence_nodes_with_loops.py
+++ sequence_nodes_with_loops.py
@@ -55,23 +55,38 @@ def move_when_even_item_numbers(left_nodes, right_nodes, embeddings_dict):
 
         return left_nodes, right_nodes
 
+def move_until_above_threshold(full_node_list, embeddings_dict):
+    """Move the nodes until a threshold is met
 
-    embeddings_list = embeddings_array.tolist()
-    return embeddings_list
-
+    Args:
+        fill_node_list (list): A list of nodes
+        embeddings_dict (dict): A dictionary that contains the embeddings
     
+    Returns:
+        list: A list containing nodes that have been moved
+    """
 
     new_list = []
+    EMBEDDINGS_FOLDER_NAME = 'embeddings'
     for node_list in full_node_list:
-        node_list_embeddings = create_sentence_embeddings(node_list, embeddings_identifier='node_list')
-        node_list_embeddings_average_similarity = calculate_average_similarity(node_list_embeddings)
+        node_list_embeddings = []
+        
+        # For a group of nodes (node_list), get embeddings
+        for i in node_list:
+            embeddings_filename = '{0}_embeddings.npy'.format(i)
+            embeddings = load_embeddings_from_file(EMBEDDINGS_FOLDER_NAME, embeddings_filename)
+            node_list_embeddings.append(embeddings)
+
+        # Calculate the average similarity of the group of nodes
+        node_list_embeddings_average_similarity = calculate_average_similarity_of_embeddings(node_list_embeddings)
         sim_threshold = 0.8
 
+        # If the average similarity between a group of nodes is less than a given similarity threshold, divide the list into two subgroups
         if node_list_embeddings_average_similarity < sim_threshold:
-            all_above_threshold = False
-
             num_nodes = len(node_list)
             cut_off_index = num_nodes // 2
+
+            # Handle even and odd number of items in node list
             if num_nodes % 2 == 0:
                 left_nodes = node_list[:cut_off_index]
                 right_nodes = node_list[cut_off_index:]
@@ -79,56 +94,87 @@ def move_when_even_item_numbers(left_nodes, right_nodes, embeddings_dict):
                 left_nodes = node_list[:cut_off_index]
                 right_nodes = node_list[cut_off_index+1:]
                 mid_node = node_list[cut_off_index]
-                mid_node_description = table_description_dict[mid_node]
-                mid_node_embeddings = create_sentence_embeddings(text_list=[mid_node_description], embeddings_identifier='mid_node')
-            left_nodes_descriptions = [table_description_dict[node] for node in left_nodes]
-            right_nodes_descriptions = [table_description_dict[node] for node in right_nodes]
-            left_nodes_embeddings = create_sentence_embeddings(text_list=left_nodes_descriptions, embeddings_identifier='contrived_2_left_nodes')
-            right_nodes_embeddings = create_sentence_embeddings(text_list=right_nodes_descriptions, embeddings_identifier='contrived_2_right_nodes')
+                mid_node_embeddings = embeddings_dict[mid_node]
 
-            left_nodes_avg_embeddings = calculate_average_embedding(left_nodes_embeddings)
-            right_nodes_avg_embeddings = calculate_average_embedding(right_nodes_embeddings)
+            # Get embeddings for nodes in left and right groups   
+            left_nodes_embeddings = [embeddings_dict[node] for node in left_nodes]
+            right_nodes_embeddings = [embeddings_dict[node] for node in right_nodes]
 
+            # Calculate the average of the embeddings of the nodes in the left and the right group
+            left_nodes_avg_embeddings = compute_average_embedding(left_nodes_embeddings)
+            right_nodes_avg_embeddings = compute_average_embedding(right_nodes_embeddings)
+
+            # Move then nodes to the group where it is more similar
             if num_nodes % 2 == 0:
-                pass
+                left_nodes, right_nodes = move_when_even_item_numbers(left_nodes, right_nodes, embeddings_dict)
+                new_list.append(left_nodes)
+                new_list.append(right_nodes)
             else:
-                mid_left_similarity_score = calculate_cosine_similarity(mid_node_embeddings, left_nodes_avg_embeddings)
-                mid_right_similarity_score = calculate_cosine_similarity(mid_node_embeddings, right_nodes_avg_embeddings)
-
+                mid_left_similarity_score = calculate_similarity_between_embeddings(mid_node_embeddings, left_nodes_avg_embeddings)
+                mid_right_similarity_score = calculate_similarity_between_embeddings(mid_node_embeddings, right_nodes_avg_embeddings)
+                
+                # If similarity score of the mid node and left nodes group is higher than that of the mid node and right nodes group,
+                # move the mid node to the left nodes. Otherwise, move it to the right nodes
                 if mid_left_similarity_score >= mid_right_similarity_score:
                     left_nodes.append(mid_node)
                 else:
-                    right_nodes.append(mid_node)
-
+                    right_nodes.insert(0, mid_node)
+            
                 new_list.append(left_nodes)
                 new_list.append(right_nodes)
         else:
-            if len(new_list) == 0:
-                new_list = node_list
-            else:
-                new_list.append(node_list)
+            new_list.append(node_list)        
     return new_list
+            
+
 if __name__ == '__main__':
     nlp = spacy.load('en_core_web_lg')
+    EMBEDDINGS_FOLDER_NAME = 'embeddings'
 
     df = pd.read_csv('contrived_descriptions.csv')
-    # Contrived
-    full_nodes_list = ['sales_1', 'sales_2', 'sales_flow_1', 'sales_flow_2', 'bill_1', 'bill_2', 'delivery_1']
 
+    # seq_nodes = ['vbrp', 'vbrk', 'bkpf', 'bseg']
+    # seq_nodes = ['vbak', 'vbap', 'vbfa', 'lips', 'likp']
+
+    # Contrived example
+    full_nodes_list = ['sales_1', 'sales_2', 'sales_flow_1', 'sales_flow_2', 'bill_1', 'delivery_1', 'delivery_2', 'delivery_3', 'purchase_1', 'purchase_2']
     full_nodes_desc = [df.loc[df['tables'] == node, 'descriptions'].iloc[0] for node in full_nodes_list]
-
+    
+    # Create dictionary of descriptions
     dict_data_records = df.to_dict(orient='records')
     table_description_dict = {item['tables']: item['descriptions'] for item in dict_data_records}
-
     
-    # Calculate the average embeddings similarity in the complete sequence
-    full_nodes_embeddings = load_or_create_embeddings(filename='contrived_2_full_nodes_embeddings.npy', embeddings_identifier='contrived_2_full_nodes', nodes=full_nodes_desc)
-    full_nodes_embeddings_average_similarity = calculate_average_similarity(full_nodes_embeddings)
-    sim_threshold = 0.8
+    # Load the embeddings model
+    model_url = "https://tfhub.dev/google/universal-sentence-encoder-large/5"
+    model = hub.load(model_url)
 
-    if full_nodes_embeddings_average_similarity < sim_threshold:
-        all_above_threshold = False
+    embeddings_dict = {}
+    full_nodes_embeddings = []
 
+    # Traverse through the dictionary and create dictionary and list of embeddings
+    for table, description in table_description_dict.items():
+        embeddings_filename = '{0}_embeddings.npy'.format(table)
+
+        # If embeddings file for a particular table exists, load the embeddings from file
+        # If embeddings file does not exist for a particular table, create embeddings and save it in a file
+        if is_file_in_subfolder(EMBEDDINGS_FOLDER_NAME, embeddings_filename):
+            embeddings = load_embeddings_from_file(EMBEDDINGS_FOLDER_NAME, embeddings_filename)
+        else:
+            embeddings = get_string_embedding(description, model)
+            save_embeddings_to_file(embeddings, folder=EMBEDDINGS_FOLDER_NAME, filename=embeddings_filename)
+
+        embeddings_dict[table] = embeddings
+        full_nodes_embeddings.append(embeddings)
+    
+    all_above_threshold = False
     while not all_above_threshold:
-        full_nodes_list = move_until_above_threshold([full_nodes_list], table_description_dict)
-        full_nodes_list = move_until_above_threshold(full_nodes_list, table_description_dict)
+        full_nodes_embeddings_average_similarity = calculate_average_similarity_of_embeddings(full_nodes_embeddings)
+        sim_threshold = 0.8
+    
+        full_nodes_list = move_until_above_threshold([full_nodes_list], embeddings_dict)
+        print('FNL 1:', full_nodes_list)
+        full_nodes_list = move_until_above_threshold(full_nodes_list, embeddings_dict)
+        print('FNL 2:', full_nodes_list)
+        full_nodes_list = move_until_above_threshold(full_nodes_list, embeddings_dict)
+        print('FNL 3:', full_nodes_list)
+        all_above_threshold = True
