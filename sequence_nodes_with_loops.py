5af90ba5afb713e027faed3ffc4483efec8d60d6
shapradhan <shapradhan@users.noreply.github.com>
2023-08-01 10:45:32 +0200

    Create a function to create embeddings

--- sequence_nodes_with_loops.py
+++ sequence_nodes_with_loops.py
@@ -1,5 +1,17 @@
 import numpy as np
+import tensorflow_hub as hub
+
 def save_embeddings_to_file(embeddings, filename):
     print("saving embedding {0}".format(filename))
     embeddings_array = np.array(embeddings)
     np.save(filename, embeddings_array)
+
+def create_sentence_embeddings(text_list, embeddings_identifier):
+    model_url = "https://tfhub.dev/google/universal-sentence-encoder-large/5"
+    model = hub.load(model_url)
+    
+    print("creating embedding {0}".format(embeddings_identifier))
+    embeddings = model(text_list)
+    save_embeddings_to_file(embeddings, "embeddings_{0}.npy".format(embeddings_identifier))
+    
+    return embeddings
