f3b219a7cc56e8caab9634a2df705c3c02e72391
shapradhan <shapradhan@users.noreply.github.com>
2023-08-22 18:39:39 +0200

    Create a function to calculate average similarity of a list of embeddings

--- utils/embeddings.py
+++ utils/embeddings.py
@@ -72,6 +72,44 @@ def compute_average_embedding(embeddings):
     average_embedding = np.mean(embeddings_array, axis=0)   # Average embedding along the first axis (axis=0)
     return average_embedding
 
+def calculate_average_similarity(embeddings):
+    """ Calculate the average cosine similarity between pairs of embeddings.
+
+    Args:
+        embeddings (list): List of embedding vectors.
+
+    Returns:
+        float: Average cosine similarity.
+
+    Example:
+        If emebddings list contains embeddings for 4 items A, B, C, and D.
+        similarity_scores matrix will look like the following.
+              A       B      C       D
+          A  1.00  (0.34)  (0.23)  (0.54)
+          B  0.34  (1.00)  (0.27)  (0.59)
+          C  0.23   0.27   (1.00)  (0.24)
+          D  0.54   0.59    0.24   (1.00)
+        We don't want the values in brackets because the similarity between A and B, for instance, is the same as similarity between B and A.
+        We also don't want the similarity between self nodes (e.g., A and A).
+
+        num_pairs = embeddings_length * embeddings_length - 1) // 2 = 4 * (4 - 1) // 2 = 6 (AB, AC, AD, BC, BD, CD)
+
+        For total_similarity = (np.sum(similarity_scores) / 2) - (embeddings_length / 2):
+            np.sum(similarity_scores) = 8.42 (which includes 1+1+1+1 of self-similarity between AA, BB, CC, and DD)
+            np.sum(similarity_scores) / 2 = 4.21 (this includes half of the self-similarity values)
+            Subtracting (embeddings_length / 2) or (4/2 = 2) removes the half of the self-similarity values
+        Therefore, average_similarity = 2.105 / 6 = 0.351
+    """
+
+    similarity_scores = cosine_similarity(embeddings)
+    embeddings_length = len(embeddings)
+
+    num_pairs = embeddings_length * (embeddings_length - 1) // 2  # Calculate the number of unique pairs
+    
+    total_similarity = (np.sum(similarity_scores) / 2) - (embeddings_length / 2)   # Exclude diagonal values (similarity to itself)
+    average_similarity = total_similarity / num_pairs
+
+    return average_similarity
 
 def calculate_similarity_between_embeddings(embedding1, embedding2):
     """Calculate similarity score between two embeddings using cosine similarity
