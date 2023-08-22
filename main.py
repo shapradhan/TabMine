02c84c13aa638bdd014863e0da04da3534e64756
shapradhan <shapradhan@users.noreply.github.com>
2023-08-22 19:02:49 +0200

    Implement community labeling and similarity score comparison

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ from db_connector import connect
 
 from community_labeler import labeler
 from connecting_node_mover import move_connecting_nodes
+from dkd_handler import calculate_similarities
 from serial_nodes_handler import detect_communities_in_node_series
 from utils.embeddings import get_embeddings_dict
 from utils.general import create_dict_from_df
-from utils.graph import arrange_nodes_in_series, convert_communities_list_to_partition, draw_graph, find_central_node, \
-  find_communities_connecting_nodes, get_edges, get_relevant_edges, group_nodes_by_community, initialize_graph, \
-  check_any_node_more_than_two_outgoing_edges, find_additional_communities
+from utils.graph import arrange_nodes_in_series, convert_communities_list_to_partition, draw_graph, find_communities_connecting_nodes, get_edges, get_relevant_edges, \
+  group_nodes_by_community, initialize_graph, check_any_node_more_than_two_outgoing_edges, find_additional_communities
 
 class ArgumentNotFoundError(Exception):
     pass
@@ -35,8 +35,11 @@ if __name__ == '__main__':
   DESCRIPTIONS_DIR = getenv('DESCRIPTIONS_DIR')
   DESCRIPTIONS_FILENAME = getenv('DESCRIPTIONS_FILENAME')
   MODEL_URL = getenv('MODEL_URL')
-  SIMILARITY_THRESHOLD = 0.5
-  EMBEDDINGS_FOLDER_NAME = 'embeddings'
+  SIMILARITY_THRESHOLD = 0.6
+  EMBEDDINGS_DIR_NAME = getenv('EMBEDDINGS_DIR')
+  DKD_EMBEDDINGS_DIR_NAME = getenv('DKD_EMBEDDINGS_DIR')
+  LABELS_DIR_NAME = getenv('LABELS_DIR')
+  DKD_DIR_NAME = getenv('DKD_DIR')
   
   cur = connect(filename = DB_CONFIG_FILENAME, section = SECTION)
   foreign_key_relation_list = get_all_fks(cur)
@@ -44,6 +47,7 @@ if __name__ == '__main__':
   nodes = [i[0] for i in get_table_names(cur)]
   edges = [get_edges(i) for i in foreign_key_relation_list]
 
+  
   # Set a random seed for reproducibility
   seed = 42
   random.seed(seed)
@@ -77,12 +81,12 @@ if __name__ == '__main__':
   model = hub.load(MODEL_URL)
 
   # Get dictionary of embeddings
-  embeddings_dict = get_embeddings_dict(descriptions_dict, model, EMBEDDINGS_FOLDER_NAME)
+  embeddings_dict = get_embeddings_dict(descriptions_dict, model, EMBEDDINGS_DIR_NAME)
 
   # communities = {'vbrp': 0, 'vbrk': 0, 'bkpf': 1, 'bseg': 1, 'likp': 2, 'lips': 2, 'vbfa': 3, 'vbap': 3, 'vbak': 3, 'nast': 4, 'cdhdr': 5, 'cdpos': 5}
   #parition = {'ekkn':0 ,'ekes':0,'eket':0,'ekbe':0,'ekko':0,'ekpo':0, 'eina':4, 'eine':4, 'mkpf':3, 'mseg':3, 'eord':5, 'bkpf':1, 'bseg':1, 'rbkp':1, 'rseg':1, 'eban':2, 'ebkn':2}
   
-  modified_partition = move_connecting_nodes(original_partition, nodes_by_community, communities_connecting_nodes, embeddings_dict)
+  modified_partition = move_connecting_nodes(original_partition, nodes_by_community, communities_connecting_nodes, embeddings_dict, SIMILARITY_THRESHOLD, G)
   # Example modified_partition = {'vbrk': 0, 'bkpf': 0, 'likp': 1, 'lips': 1, 'bseg': 0, 'vbak': 1, 'vbap': 1, 'cdhdr': 2, 'nast': 3, 'cdpos': 2, 'vbfa': 1, 'vbrp': 0}
   print('Modified partition after applying first arrangement: {0}'.format(modified_partition))
 
@@ -109,12 +113,12 @@ if __name__ == '__main__':
   else:
     # If no node has more than two outgoing edges
 
-    for community_id, nodes in nodes_by_community.items():
-      print('arranging node {0} in a series'.format(nodes))
-      if len(nodes) > 1:
+    for community_id, nodes_in_community in nodes_by_community.items():
+      print('arranging node {0} in a series'.format(nodes_in_community))
+      if len(nodes_in_community) > 1:
         # Get the central node and relevant edges from nodes in a community
-        central_node = find_central_node(nodes, sources, targets, G, centrality_measure='betweenness')
-        relevant_edges = get_relevant_edges(nodes, edges)
+        # central_node = find_central_node(nodes, sources, targets, G, centrality_measure='betweenness')
+        relevant_edges = get_relevant_edges(nodes_in_community, edges)
 
         # If there are more than one relevant edge, apply arrange_nodes_in_series to get a proper order of the nodes.
         # Otherwise, append the nodes to node_chain. 
@@ -123,9 +127,9 @@ if __name__ == '__main__':
           nodes_in_series = arrange_nodes_in_series(relevant_edges)
           node_chains.append(nodes_in_series)
         else:
-          node_chains.append(nodes)
+          node_chains.append(nodes_in_community)
       else:
-        node_chains.append(nodes)
+        node_chains.append(nodes_in_community)
     
     # Loop through each chain in node_chains
     # Example node_chains = [
@@ -154,7 +158,16 @@ if __name__ == '__main__':
 
   new_G = initialize_graph(nodes, edges, directed=False)
 
-  community_labels = labeler(final_partition, descriptions_dict, embeddings_dict)
+  community_labels = labeler(final_partition, descriptions_dict)
   # Example community_labels = {0: 'bill', 1: 'account', 2: 'sd delivery', 3: 'sale', 4: 'message status', 5: 'change'}
 
   draw_graph(new_G, final_partition, title='Modified Communities - Undirected - Louvain', labels= community_labels, color_map='Pastel1')
+
+  # DKD Handling
+  dkd_file_path = path.join(DKD_DIR_NAME, dkd_filename)
+
+  sim_score_df = calculate_similarities(community_labels, model, dkd_file_path, DKD_EMBEDDINGS_DIR_NAME, LABELS_DIR_NAME)
+  sorted_df = sim_score_df.sort_values(by=['document', 'similarity_score'], ascending=[True, False])
+
+  print('Document-Label Matching')
+  print(sorted_df)
