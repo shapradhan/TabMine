01b042a629d1cf260e86b3eec8cfb9d03801fb29
shapradhan <shapradhan@users.noreply.github.com>
2024-04-25 22:08:30 +0200

    Fix docstrings and remove unused functions

--- utils/general.py
+++ utils/general.py
@@ -1,9 +1,9 @@
 import os
 import re
-from collections import Counter
 
 def make_subdirectory(subdirectory_name):
-    """Creates a new sub directory with the specified name within the current directory.
+    """
+    Create a new sub directory with the specified name within the current directory.
 
     Args:
     subdirectory_name (str): The name of the subdirectory to be created.
@@ -20,7 +20,7 @@ def make_subdirectory(subdirectory_name):
 
 def reset_community_id_numbers(partition):
     """
-    Resets the community ID in a partition dictionary as the algorithm to move connector nodes assigns higher ID values.
+    Reset the community ID in a partition dictionary as the algorithm to move connector nodes assigns higher ID values.
     
     Args:
         partition (dict): A dictionary representing a partition where keys are nodes and values are community IDs.
@@ -49,7 +49,7 @@ def reset_community_id_numbers(partition):
 
 def extract_substring_between_strings(text, start_str, end_str):
     """
-    Extracts the substring located between two specified strings in the given text.
+    Extract the substring located between two specified strings in the given text.
 
     Args:
         text (str): The input text from which the word will be extracted.
@@ -71,7 +71,8 @@ def extract_substring_between_strings(text, start_str, end_str):
     return None
 
 def is_file_in_subdirectory(subdirectory_name, filename):
-    """Check if a file with a given filename is in a sub directory within the current direction,
+    """
+    Check if a file with a given filename is in a sub directory within the current direction,
 
     Args:
         subdirectory_name (str): The name of the sub directory.
@@ -79,100 +80,83 @@ def is_file_in_subdirectory(subdirectory_name, filename):
     
     Returns:
         bool: True if the file is in the sub directory, Otherwise, False.
+    
+    Example:
+        >>> is_file_in_subdirectory('subdir', 'file.txt')
+        True
     """
+
     file_path = os.path.join(os.getcwd(), subdirectory_name, filename)
     return os.path.isfile(file_path)
 
-
-def create_dict_from_df(df, key_col, value_col):
-    """ Create a dictionary from a given DataFrame with given columns as keys and values.
-
-    Args:
-        df (pandas.DataFrame): The DataFrame from which a dictionary has to be created.
-        key_col (str): The name of the column of the DataFrame that will be used as the dictionary key.
-        value_col (str): The name of the column of the DataFrame that will be used as the dictionary value.
-    
-    Returns:
-        dict: A dictionary containing values from the given DataFrame as keys and values.
+def read_lines(filename):
     """
-
-    dict_data_records = df.to_dict(orient='records')
-    return {item[key_col]: item[value_col] for item in dict_data_records}
-
-
-def get_multiple_occuring_values(lst):
-    """ Find and return values that occur more than once in a list.
+    Read lines from a text file and return them as a list.
 
     Args:
-        lst (list): The input list containing values to be analyzed.
+        filename (str): The path to the text file.
 
     Returns:
-        list: A list of values that occur more than once in the input list.
+        list: A list containing the lines read from the text file.
 
     Example:
-        lst = [1, 2, 2, 3, 4, 4, 4, 5]
-        Returns [2, 4]
+        >>> lines = read_lines('example.txt')
     """
+
+    with open(filename, 'r') as file:
+        return [line.strip() for line in file.readlines()]
     
-    # Count occurrences of each element
-    element_counts = Counter(lst)
+def contains_value(list_of_tuples, value):
+    """
+    Check if any tuple in the list contains the specified value.
 
-    # Get values that occur multiple times
-    values_with_multiple_occurrences = [value for value, count in element_counts.items() if count > 1]
-
-    return values_with_multiple_occurrences
-
-
-def get_key_by_value(dictionary, value):
-    """ Get the key in the given dictionary based on the specified value.
     Args:
-        dictionary (dict): The dictionary to search in.
-        value: The value to search for in the dictionary.
+        list_of_tuples (list of tuples): The list of tuples to search.
+        value: The value to search for within the tuples.
 
     Returns:
-        The key corresponding to the given value, if found; otherwise, returns None.
+        bool: True if the value is found in any tuple, False otherwise.
     """
+
+    return any(value in a for a in list_of_tuples)
+
+def find_keys_with_value(dictionary, target_value):
+    """
+    Find keys in a dictionary that have a specified value.
+
+    Args:
+        dictionary (dict): The dictionary to search.
+        target_value: The value to search for within the dictionary.
+
+    Returns:
+        list: A list of keys in the dictionary that have the specified value.
+    """
+
+    return [key for key, value in dictionary.items() if value == target_value]
+
+def append_unique_list(main_list, new_list):
+    """
+    Append elements from a new list to a main list if they are not already present.
+
+    Args:
+        main_list (list): The main list to which unique elements will be appended.
+        new_list (list): The list containing elements to be appended to the main list.
+
+    Returns:
+        None: The function modifies the main list in place.
+
+    Example:
+        main_list = [1, 2, 3]
+        new_list = [3, 4, 5]
+        append_unique_list(main_list, new_list)
+        print(main_list)  # Output: [1, 2, 3, 4, 5]
+    """
+
+    # Convert the lists to sets for comparison
+    main_set = {tuple(sublist) for sublist in main_list}
+    new_set = set(new_list)
     
-    for key, val in dictionary.items():
-        if val == value:
-            return key
-    return None 
-
-
-def check_value_in_list(value1, value2, lst):
-    """ Check the presence of value1 or value2 in the given list.
-
-    Args:
-        value1: The first value to search for.
-        value2: The second value to search for.
-        lst (list): The list to search in.
-
-    Returns:
-        str or None: value1 if it is present in the given list or value2 if it is present in the given list.
-            None if neither of the given values are present in the given list.
-    """
-
-    return value1 if value1 in lst else (value2 if value2 in lst else None)
-
-def find_most_common_words(word_list):
-    """ Find the most common words in a given list of words.
-
-    Args:
-        word_list (list): A list of words to analyze.
-
-    Returns:
-        list: A list of the most common words (can contain multiple words).
-    """
-
-    joined_texts = ' '.join(word_list)
-
-    # Tokenize and preprocess the text
-    words = re.findall(r'\w+', joined_texts) 
-
-    word_counts = Counter(words)
-    most_common_count = word_counts.most_common(1)[0][1]
-
-    # Get all words that have the same count as the most common count
-    most_common_words = [word for word, count in word_counts.items() if count == most_common_count]
-
-    return most_common_words
+    # If the new_list is not already in the main_list, append it
+    if tuple(new_set) not in main_set:
+        main_list.append(list(new_set))
+    return main_list
