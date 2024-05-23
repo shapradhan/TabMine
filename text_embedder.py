44df7ce7e53b1e5027dbebbaa349bfaed9ee3fca
shapradhan <shapradhan@users.noreply.github.com>
2024-05-23 15:28:22 +0200

    Add check for usage of OpenAI and add docstring

--- text_embedder.py
+++ text_embedder.py
@@ -1,31 +1,76 @@
-import os
-import numpy as np
-import re
+import os, numpy as np, re
 
+from openai import AzureOpenAI
 from utils.general import make_subdirectory
 
 class TextEmbedder:
     def __init__(self, text='', model=None):
+        """
+        Initialize the instance with the provided text and model.
+
+        This constructor method sets up an instance of the class with optional text and model parameters.
+        The text parameter can be used to provide initial text data, while the model parameter can be used
+        to specify a pre-trained model for embeddings generation.
+
+        Args:
+        text (str, optional):  The initial text data to be associated with the instance. The default is an empty string.
+        
+        model (str or SentenceTransformer, optional): A pre-trained model to be used by the instance for various operations. This can be either a
+            string representing the model's name or a `SentenceTransformer` object. The default is None.
+
+        Returns:
+            None
+
+        Example:
+            >>> instance = TextEmbedder(text='Sample text', model=pretrained_model)
+            >>> print(instance.text)
+            'Sample text'
+            >>> print(instance.model)
+                <pretrained_model>
+        """
+
         self.text = text
         self.model = model
    
-    def create_embeddings(self):
-        """Retrieves embeddings for a given input string using the specified embedding model.
+    def create_embeddings(self, use_openai=False):
+        """
+        Generate embeddings for a given input string.
 
         Args:
-            input_string (str): The input string for which embeddings are to be generated.
-            embedding_model (callable): A callable object representing the embedding model. The model must take
-                a list of input strings and return a list of embeddings for those strings.
+            use_openai (bool, optional): A flag indicating whether to use the OpenAI API to generate the embeddings. 
+                If True, the embeddings will be generated using OpenAI's model. If False, an alternative embedding method will be used. 
+                The default is False.
 
-        Returns: 
-            tf.Tensor: A TensorFlow tensor containing the embeddings for the input string.
+        Returns:
+            list[float] or tf.Tensor: A list of floats or a TensorFlow tensor containing the embeddings for the input string.
+
+        Example:
+            >>> embeddings = self.create_embeddings(use_openai=True)
+            >>> print(embeddings)
+            {0: [0.1, 0.2, 0.3], 1: [0.4, 0.5, 0.6], ...}
+        
+        Note:
+           - If `use_openai` is False, ensure that an alternative embedding method is implemented and accessible within the class.
         """
 
         self.text = re.split(r'[.!?]', self.text)
         self.text = [s.strip() for s in self.text if s.strip()]
-        
-        # Compute embeddings for the input sentences
-        embeddings = self.model.encode(self.text, convert_to_tensor=True)
+
+        if use_openai:
+            client = AzureOpenAI(
+                api_key = os.getenv("AZURE_OPENAI_API_KEY"),  
+                api_version = os.getenv("AZURE_OPENAI_VERSION"),  
+                azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT") 
+            )      
+
+            response = client.embeddings.create(
+                input = self.text,
+                model = os.getenv("OPENAI_MODEL_NAME") 
+            )
+
+            embeddings = response.data[0].embedding
+        else:
+            embeddings = self.model.encode(self.text, convert_to_tensor=True)
         return embeddings
 
     def save_embeddings(self, embeddings, folder_name, filename):
@@ -37,7 +82,7 @@ class TextEmbedder:
             filename (str): The name of the file to be created.
         
         Returns:
-        
+            None
         """
         
         make_subdirectory(folder_name)
