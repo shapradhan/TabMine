fa74b0f3461b6f7541440e260250acc6c5507959
shapradhan <shapradhan@users.noreply.github.com>
2024-05-23 16:57:28 +0200

    Create a function to perform breadth-first search

--- sub_graph_analyzer.py
+++ sub_graph_analyzer.py
@@ -1,3 +1,4 @@
+from collections import deque
 class SubGraphAnalyzer:
     def __init__(self, subgraph):
         """
@@ -22,5 +23,51 @@ class SubGraphAnalyzer:
             - After initialization, the `partition` dictionary will be used to store the community assignments for nodes in the subgraph.
         """
         
+
         self.graph = subgraph
         self.partition = {}
+
+    def _bfs_with_exclusion(self, start_node, exclude_node=None, ignored_nodes=None):
+        """
+        Perform a breadth-first search (BFS) traversal on a graph, optionally excluding specific nodes and ignoring others.
+
+        Args:
+        - start_node (str): The node from which the BFS traversal begins.
+        - exclude_node (str, optional): A node to be excluded from traversal. Defaults to None.
+        - ignored_nodes (lsit, optional): A list of nodes to be ignored during traversal. Defaults to None.
+
+        Returns:
+        - list: A list containing the nodes visited during BFS traversal.
+
+        Example:
+            >>> graph = {'A': ['B', 'C'], 'B': ['A', 'D', 'E'], 'C': ['A', 'F'], 'D': ['B'], 'E': ['B', 'F'], 'F': ['C', 'E']}
+            >>> analyzer = SubGraphAnalyzer(graph)
+            >>> result = analyzer.bfs_with_exclusion_and_ignore('A', exclude_node='C', ignored_nodes=['B', 'F'])
+            >>> print(result)
+            ['A', 'D', 'E']
+        
+        Note:
+            - This method is intended for internal use within the class and may not be directly accessible from outside the class.
+        """
+        
+        visited = set()
+        queue = deque([start_node])
+        result = []
+
+        while queue:
+            node = queue.popleft()
+            if node in visited:
+                continue
+
+            # Check if the current node should be excluded or ignored
+            if node == exclude_node or (ignored_nodes and node in ignored_nodes):
+                continue
+
+            visited.add(node)
+            result.append(node)
+
+            for neighbor in self.graph.neighbors(node):
+                if neighbor not in visited:
+                    queue.append(neighbor)
+
+        return result
