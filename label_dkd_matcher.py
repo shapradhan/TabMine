368f09033979b0a922200e52a61440be32f6bc42
shapradhan <shapradhan@users.noreply.github.com>
2024-05-23 17:48:18 +0200

    Create a function to read the Domain Knowledge Definition file and get the list of documents from it

--- label_dkd_matcher.py
+++ label_dkd_matcher.py
@@ -1,3 +1,4 @@
+import json
 class Matcher:
     def __init__(self):
         """
@@ -16,6 +17,28 @@ class Matcher:
         Note:
             - After initialization, `documents` will be set to None and `labels` will be an empty dictionary.
         """
-        
+
         self.documents = None
         self.labels = {}
+    
+    def get_documents_from_dkd(self, dkd_filename):
+        """
+        Read documents from the Domain Knowledge Definition (DKD) file
+
+        Args:
+            dkd_filename (str): The name of the DKD file.
+        
+        Returns:
+            None
+        
+        Example:
+            >>> filename = 'dkd.json'
+            >>> matcher = Matcher()
+            >>> matcher.get_documents_from_dkd(filename)
+        
+        Note:
+            - The DKD file must be a JSON file that adheres to the standard DKD structure.
+        """
+
+        with open(dkd_filename, 'r') as file:
+            self.documents = json.load(file)['documents']
