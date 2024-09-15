662876502d4a1224963a4e0e15a861094001651b
Sha Pradhan <shapradhan@users.noreply.github.com>
2024-09-15 19:00:09 +0200

    Remove reference to use_openai and modify docstring

--- label_dkd_matcher.py
+++ label_dkd_matcher.py
@@ -77,8 +77,9 @@ class Matcher:
                 label = row[1]
 
                 self.labels[community_id] = label
+        
     
-    def _load_or_create_embeddings(self, text, dir, embeddings_dict, model, use_openai):
+    def _load_or_create_embeddings(self, text, dir, embeddings_dict, model):
         """
         Load the embeddings if they exists; otherwise, create embeddings.
 
@@ -87,8 +88,6 @@ class Matcher:
             dir (str): The path of the directory in which the embeddings may exists.
             embeddings_dict (dict): An empty dictionary.
             model (str or SentenceTransformer): The name of the model if OpenAI is to be used or a SentenceTransformer model.
-            use_openai (bool): A flag indicating whether to use the OpenAI API to generate the embeddings. 
-                If True, the embeddings will be generated using OpenAI's model. If False, an alternative embedding method will be used. 
         
         Returns:
             dict: A dictionary in which the key represents either the document from Domain Knowledge Definition file or the community label and the
@@ -106,20 +105,18 @@ class Matcher:
             embeddings_dict[text] = embedder.load_embeddings_from_file(dir, embeddings_filename)
         else:
             embedder = TextEmbedder(text, model)
-            embeddings = embedder.create_embeddings(use_openai)
+            embeddings = embedder.create_embeddings()
             embeddings_dict[text] = embeddings
             embedder.save_embeddings(embeddings, dir, embeddings_filename)
             
         return embeddings_dict
 
-    def _get_embeddings(self, model, use_openai):
+    def _get_embeddings(self, model):
         """
         Get embeddings for the documents from Domain Knowledge Definition file and the labels assigned to the communities.
 
         Args:
             model (str or SentenceTransformer): The name of the model if OpenAI is to be used or a SentenceTransformer model.
-            use_openai (bool): A flag indicating whether to use the OpenAI API to generate the embeddings. 
-                If True, the embeddings will be generated using OpenAI's model. If False, an alternative embedding method will be used. 
         
         Returns:
             tuple: A tuple consisting of two values.
@@ -137,31 +134,44 @@ class Matcher:
         LABEL_EMBEDDINGS_DIR = getenv('LABEL_EMBEDDINGS_DIR')
 
         for doc in self.documents:
-            self._load_or_create_embeddings(doc, DOCUMENT_EMBEDDINGS_DIR, document_embeddings, model, use_openai)
+            self._load_or_create_embeddings(doc, DOCUMENT_EMBEDDINGS_DIR, document_embeddings, model)
         
         for label in self.labels.values():
-            self._load_or_create_embeddings(label, LABEL_EMBEDDINGS_DIR, label_embeddings, model, use_openai)
+            self._load_or_create_embeddings(label, LABEL_EMBEDDINGS_DIR, label_embeddings, model)
         
         return document_embeddings, label_embeddings
     
 
-    def compute_similarity_scores(self, model, similarity_measure, use_openai):
+    def compute_similarity_scores(self, model, similarity_measure):
         """
-        Compute similarity scores between the documents from Domain Knowledge Definition file and the labels assigned to the communities.
+        Compute similarity scores between documents from the Domain Knowledge Definition file and labels assigned to the communities.
 
         Args:
-            model (str or SentenceTransformer): The name of the model if OpenAI is to be used or a SentenceTransformer model.
-            use_openai (bool): A flag indicating whether to use the OpenAI API to generate the embeddings. 
-                If True, the embeddings will be generated using OpenAI's model. If False, an alternative embedding method will be used. 
-        
+            model (str or SentenceTransformer): The model used for computing similarity. This can be:
+                - A string representing the name of the OpenAI model to use.
+                - An instance of SentenceTransformer for other similarity computations.
+            similarity_measure (function): A function or callable used to calculate the similarity score between documents and community labels. This function should take two inputs (a document and a label) and return a similarity score.
+
         Returns:
-            dict: A dictionary of dictionaries. The keys in the inner dictionary represents the community labels and the values represent the 
-                similarity score between that label and the key of the outer dictionary i.e, the documents.
+            dict: A dictionary where:
+                - The keys are document identifiers (e.g., document names or IDs).
+                - The values are dictionaries with community labels as keys and their corresponding similarity scores as values. Each inner dictionary represents the similarity scores between a particular document and each community label.
+
+        Example:
+            {
+                'doc1': {
+                    'labelA': 0.85,
+                    'labelB': 0.75
+                },
+                'doc2': {
+                    'labelA': 0.90,
+                    'labelB': 0.65
+                }
+            }
         """
         
         similarity_scores = {}
-        document_embeddings, label_embeddings = self._get_embeddings(model, use_openai)
-
+        document_embeddings, label_embeddings = self._get_embeddings(model)
         for doc, doc_emb in document_embeddings.items():
             temp_dict = {}
             for label, label_emb in label_embeddings.items():
