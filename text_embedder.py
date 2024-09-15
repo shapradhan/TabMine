8f3a9db32e8bbe2c8bfd47da94955bdc579e1bcb
Sha Pradhan <shapradhan@users.noreply.github.com>
2024-09-15 19:11:11 +0200

    Modify docstrings

--- text_embedder.py
+++ text_embedder.py
@@ -13,9 +13,9 @@ class TextEmbedder:
         to specify a pre-trained model for embeddings generation.
 
         Args:
-        text (str, optional):  The initial text data to be associated with the instance. The default is an empty string.
+            - text (str, optional):  The initial text data to be associated with the instance. The default is an empty string.
         
-        model (str or SentenceTransformer, optional): A pre-trained model to be used by the instance for various operations. This can be either a
+            - model (str or SentenceTransformer, optional): A pre-trained model to be used by the instance for various operations. This can be either a
             string representing the model's name or a `SentenceTransformer` object. The default is None.
 
         Returns:
@@ -32,31 +32,28 @@ class TextEmbedder:
         self.text = text
         self.model = model
    
-    def create_embeddings(self, use_openai=False):
+    def create_embeddings(self):
         """
-        Generate embeddings for a given input string.
-
-        Args:
-            use_openai (bool, optional): A flag indicating whether to use the OpenAI API to generate the embeddings. 
-                If True, the embeddings will be generated using OpenAI's model. If False, an alternative embedding method will be used. 
-                The default is False.
+        Generate embeddings for a given input string using the specified model or embedding method.
 
         Returns:
-            list[float] or tf.Tensor: A list of floats or a TensorFlow tensor containing the embeddings for the input string.
+            - list[float] or tf.Tensor: A list of floats or a TensorFlow tensor containing the embeddings for the input string. 
+                The type depends on the model used for generating the embeddings.
 
         Example:
-            >>> embeddings = self.create_embeddings(use_openai=True)
+            >>> embeddings = self.create_embeddings()
             >>> print(embeddings)
-            {0: [0.1, 0.2, 0.3], 1: [0.4, 0.5, 0.6], ...}
-        
-        Note:
-           - If `use_openai` is False, ensure that an alternative embedding method is implemented and accessible within the class.
+            [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, ...]
+
+            >>> embeddings = self.create_embeddings()
+            >>> print(embeddings)
+            <tf.Tensor: shape=(..., ...), dtype=float32, numpy=...>
         """
 
         self.text = re.split(r'[.!?]', self.text)
         self.text = [s.strip() for s in self.text if s.strip()]
 
-        if use_openai:
+        if self.model == 'gpt3.5':
             client = AzureOpenAI(
                 api_key = os.getenv("AZURE_OPENAI_API_KEY"),  
                 api_version = os.getenv("AZURE_OPENAI_VERSION"),  
@@ -69,22 +66,19 @@ class TextEmbedder:
             )
 
             embeddings = response.data[0].embedding
-            
-            Embed4All
-            embedder = Embed4All()
-            embeddings = embedder.embed(self.text)
 
         else:
             embeddings = self.model.encode(self.text, convert_to_tensor=True)
         return embeddings
 
+
     def save_embeddings(self, embeddings, folder_name, filename):
         """Saves the embeddings to a file in a given folder
 
         Args:
-            embeddings (tf.Tensor):  A TensorFlow EagerTensor containing the embeddings to be saved.
-            folder_name (str): The name of a folder where the file will be saved
-            filename (str): The name of the file to be created.
+            - embeddings (tf.Tensor):  A TensorFlow EagerTensor containing the embeddings to be saved.
+            - folder_name (str): The name of a folder where the file will be saved
+            - filename (str): The name of the file to be created.
         
         Returns:
             None
@@ -98,11 +92,11 @@ class TextEmbedder:
         """Loads the embeddings of a particular file stored in a subfolder
         
         Args:
-            subfolder_name (str): The name of the subfolder
-            filename (str): The name of the file in which the embeddings are stored
+            - subfolder_name (str): The name of the subfolder
+            - filename (str): The name of the file in which the embeddings are stored
 
         Returns:
-            numpy.ndarray: an array of embeddings
+            - numpy.ndarray: an array of embeddings
         """
 
         file_path = os.path.join(os.getcwd(), subfolder_name, filename)
